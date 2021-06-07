757176f5f9cc3de2d728631f601a1ac1b14a42b8
jasonpark3306 <jason.park@altibase.com>
2021-06-07 17:35:15 +0900

    BMT test set upload

--- 1.BMT/Test4Single/work/HDB/APRE/MEMORY/clean.sh
+++
@@ -1,4 +0,0 @@
-server stop
-rm -f $ALTIBASE_HOME/dbs/* $ALTIBASE_HOME/logs/*
-server create US7ASCII UTF16
-server start
--- 1.BMT/Test4Single/work/HDB/APRE/MEMORY/usagestart.sh
+++
@@ -1,4 +0,0 @@
-rm -f ./usage_collect
-INTERVAL=0.5
-DBPID=`ps -ef | grep $USER | grep "altibase -p" | grep -v grep | awk '{print $2}'`
-top -d $INTERVAL -p $DBPID -b > ./usage_collect &
